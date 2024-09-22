7012eac538354e43322a6e39b753811dcbd187c9
Dhairya Khanna <dk9582671597@gmail.com>
2024-09-22 14:49:56 +0530

    feat: adding methods for creating cache

--- pokeCache/pokeCache.go
+++ pokeCache/pokeCache.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 	"time"
 )
+
 type Cache struct {
 	cache    map[string]cacheEntry
 	mux      *sync.RWMutex
@@ -12,4 +13,16 @@ type Cache struct {
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	return &Cache{
+		cache:    map[string]cacheEntry{},
+		mux:      &sync.RWMutex{},
+		interval: interval,
+	}
+}
+func (c *Cache) Add(key string, val []byte) {
+	c.cache[key] = cacheEntry{val: val, createdAt: time.Now()}
+
 }
