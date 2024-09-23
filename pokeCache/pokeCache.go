8347a82dd78851f10dabfaaa028291c3cc84ca10
Dhairya Khanna <dk9582671597@gmail.com>
2024-09-23 23:12:50 +0530

    feat: added Reaploop and mutex

--- pokeCache/pokeCache.go
+++ pokeCache/pokeCache.go
@@ -23,13 +23,31 @@ func NewCache(interval time.Duration) *Cache {
 	}
 }
 func (c *Cache) Add(key string, val []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	c.cache[key] = cacheEntry{val: val, createdAt: time.Now()}
 
 }
 func (c* Cache)Get(key string)([]byte,bool){
+	c.mux.RLock()
+	defer c.mux.RUnlock()
 	entry,ok:=c.cache[key]
 	if !ok{
 		return []byte{},false
 	}
 	return entry.val,true
+}
+func (ch *Cache) ReapLoop() {
+	ticker := time.NewTicker(ch.interval)
+	defer ticker.Stop()
+	for {
+		<-ticker.C
+		ch.mux.Lock()
+		for key, entry := range ch.cache {
+			if time.Since(entry.createdAt) > ch.interval {
+				delete(ch.cache, key)
+			}
+		}
+		ch.mux.Unlock()
+	}
 }
